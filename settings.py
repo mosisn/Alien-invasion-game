bd47242a6661ced49a2e2c984d8f46e865daee90
Mostafa <mostafa.saneisn@gmail.com>
2024-01-19 10:58:44 +0330

    feat: adding bullets and shooting!

--- settings.py
+++ settings.py
@@ -1,6 +1,13 @@
 class Settings:
     def __init__(self) -> None:
         self.screen_height = 800
-        self.screen_width = 800
+        self.screen_width = 1200
         self.bg_color = (230, 230, 230)
-        self.ship_speed = 1.5
+
+        self.ship_speed = 2.5
+
+        self.bullet_speed = 4.5
+        self.bullet_height = 15
+        self.bullet_width = 5
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
